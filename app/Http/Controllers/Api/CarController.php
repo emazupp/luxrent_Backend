ea15119f5fa9201efe0ce92a988c81d5d69f12f4
emazupp <zuppardo.emanuele2001@gmail.com>
2025-04-05 21:34:15 +0200

    👽️ API: added index brands

--- app/Http/Controllers/Api/CarController.php
+++ app/Http/Controllers/Api/CarController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Car;
 use Illuminate\Http\Request;
 
+
 class CarController extends Controller
 {
     public function index()
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\BrandController;
 use App\Http\Controllers\Api\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/cars", [CarController::class, "index"]);
 Route::get("/cars/{car}", [CarController::class, "show"]);
+
+Route::get("/brands", [BrandController::class, "index"]);
+
